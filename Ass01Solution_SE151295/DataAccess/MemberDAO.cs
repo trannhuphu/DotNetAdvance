29bf48bf44bc7a2f1141874938e87cf657794971
phutran1995 <trannhuphu1995@gmail.com>
2022-05-14 21:17:35 +0700

    Update MemberDAO

--- Ass01Solution_SE151295/DataAccess/MemberDAO.cs
+++ Ass01Solution_SE151295/DataAccess/MemberDAO.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessObject.Models;
+using Microsoft.EntityFrameworkCore; 
 
 namespace DataAccess
-{
-    class MemberDAO
-    {
+{ 
+    public class MemberDAO {
+        
+        private static MemberDAO instance = null; 
+        private static readonly object instanceLock = new object(); 
+        private MemberDAO() { } 
+        public static MemberDAO Instance
+        { 
+            get{ 
+                lock (instanceLock) 
+                { 
+                    if (instance == null){
+                        instance = new MemberDAO();
+                    }
+                    return Instance;
+                }
+            }
+        }
     }
 }
+
+ 
