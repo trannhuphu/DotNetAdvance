35da905f77e58dfe0e793e9c3bba0c401304d2a0
phutran1995 <trannhuphu1995@gmail.com>
2022-05-30 21:08:40 +0700

    Update page

--- ShoppingAssignment_SE151295/ShoppingAssignment_SE151295/Pages/Login/MyLogin.cshtml.cs
+++ ShoppingAssignment_SE151295/ShoppingAssignment_SE151295/Pages/Login/MyLogin.cshtml.cs
@@ -4,25 +4,41 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using ShoppingAssignment_SE151295.Models;
 
 namespace ShoppingAssignment_SE151295.Pages.Login
 {
     public class MyLoginModel : PageModel
     {
-        private readonly ShoppingAssignment_SE151295.Models.NorthwindCopyDBContext _DBcontext;
+        private readonly ShoppingAssignment_SE151295.Models.NorthwindCopyDBContext _context;
        
-        public MyLoginModel(NorthwindCopyDBContext DBcontext)
+        public MyLoginModel(NorthwindCopyDBContext context)
         {
-            _DBcontext = DBcontext;
+            _context = context;
         }
 
         [BindProperty (SupportsGet = true)]
-        public string MyName {set; get;}
+        public Customer CusLogin { get; set; }
 
         public void OnGet()
         {
-         
+        }
+
+        public string ErrorMsg {set;get;}
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            Customer CusTmp = await _context.Customers.FirstOrDefaultAsync(m => m.Email == CusLogin.Email 
+                && m.Password == CusLogin.Password);
+
+            if(CusTmp == null)
+            {
+                ErrorMsg = "Email or Passowrd are incorrect";
+                return Page();
+            }
+
+            return RedirectToPage("/Customers/Index");
         }
     }
 }
